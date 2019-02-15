ab39a86e5bb4bb3aca22b934ecae16b44303e46b
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2019-02-15 15:01:27 +0300

    Added power up zones. Also changed the stiffness of forward and sideways friction a bit.

---
+++ Assets/Scripts/PowerUpSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawn : MonoBehaviour
+{
+    public List<GameObject> powerUps;// gameobject that contains all the power ups as a child
+    public int powerUpSpawnCount = 4; // how many power ups will spawn in one spawn point
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < GameObject.FindGameObjectWithTag("PowerUp").transform.childCount; i++)
+        {
+            powerUps.Add(GameObject.FindGameObjectWithTag("PowerUp").transform.GetChild(i).gameObject);
+        };
+        //GameObject.FindGameObjectWithTag("PowerUp").SetActive(false);
+        Debug.Log(powerUps[0]);
+
+        for (int i = 0; i < powerUpSpawnCount; i++)
+        {
+            GameObject currentGameobject = GameObject.Instantiate(powerUps[Random.Range(0, powerUps.Count)], transform);
+            currentGameobject.SetActive(true);
+            currentGameobject.transform.localPosition = new Vector3(i * 5, 0, 0);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
