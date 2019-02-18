692893ff62315b689aa332e3ee7f2e63c40a1c7e
HARDLINE\Anthony <lordrahvin@gmail.com>
2019-02-17 20:12:09 -0800

    Cameras are more stable.  Camera stays parallel to ground instead of locking to car's rotation.

---
+++ Assets/CameraLock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+	private float yVal = 0f;
+	Vector3 dir = Vector3.zero;
+
+	void LateUpdate()
+    {
+		yVal = transform.rotation.eulerAngles.y;
+		dir = new Vector3(0f, yVal, 0f);
+		transform.rotation = Quaternion.Euler(dir);
+    }
+}
