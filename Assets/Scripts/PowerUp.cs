51ebd6008eb91f91d598a827653de5bb87b94d97
deepbluezen <deepbluezen@users.noreply.github.com>
2019-02-20 21:37:45 +0000

    Missile collectable start, scripts moved to scripts folder

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public Transform AttachPoint;
+
+    void OnTriggerEnter(Collider pickup)
+    {
+        if (pickup.gameObject.CompareTag("Pickup"))
+        {
+            pickup.gameObject.SetActive(false);
+
+            Instantiate(rocketPrefab,AttachPoint.position, AttachPoint.rotation, AttachPoint.parent);
+            
+        }
+    }
+}
