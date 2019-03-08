a5bbdab2b84a2220509e6f2b2770f493ab1a2757
Barış Köklü <bariskoklu@gmail.com>
2019-03-08 15:01:37 +0300

    Basic main menu and game over menu added. Options and credits buttons does not work. Will improve it with the progresses of the game.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void Options()
+    {
+        Debug.Log("Options button clicked");
+    }
+    public void Credits()
+    {
+        Debug.Log("Credits button clicked");
+    }
+}
