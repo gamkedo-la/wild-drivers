2f600fb4867e99650cf32696479cdaddd21495c8
Barış Köklü <bariskoklu@gmail.com>
2019-03-09 15:29:36 +0300

    Some trees added to side of the roads. Also those added random rotation to the some trees

---
+++ Assets/Scripts/TreeRandomTransform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomTransform : MonoBehaviour
+{
+    public int randomTransformY;
+    public Vector3 tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomTransformY = Random.Range(0, 360);
+        tr = new Vector3(0.0000f, randomTransformY, 0.0000f);
+        transform.rotation = Quaternion.Euler(tr);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
