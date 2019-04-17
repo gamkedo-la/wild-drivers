5d30332336d43f6e308f814b6aaee8a6ff3b15d7
Barış Köklü <bariskoklu@gmail.com>
2019-04-17 20:13:53 +0300

    Bike wheels rotate now.

--- Assets/Scripts/BikeRotation.cs
+++ Assets/Scripts/BikeRotation.cs
@@ -9,11 +9,21 @@ public class BikeRotation : MonoBehaviour
     public BikeDrive bikeDriveScript;
     public float horizontalInput;
     public float bikeAcceleration;
+    public GameObject wheels;
+    private float speedInKPH;
     void Start()
     {
         bikeDriveScript = gameObject.GetComponentInParent<BikeDrive>();
         bikeAcceleration = bikeDriveScript.acceleration;
         horizontalInput = bikeDriveScript.horizontalInput;
+    }
+    private void FixedUpdate()
+    {
+        speedInKPH = Mathf.Round((gameObject.GetComponent<Rigidbody>().velocity.magnitude) * 3.6f);
+        for (int i = 0; i < wheels.transform.childCount; i++)
+        {
+            wheels.transform.GetChild(i).localEulerAngles = new Vector3(wheels.transform.GetChild(i).localEulerAngles.x + speedInKPH*10, wheels.transform.GetChild(i).localEulerAngles.y, wheels.transform.GetChild(i).localEulerAngles.z);
+        }
     }
 
     // Update is called once per frame
@@ -22,5 +32,6 @@ public class BikeRotation : MonoBehaviour
         bikeAcceleration = bikeDriveScript.acceleration;
         horizontalInput = gameObject.GetComponentInParent<BikeDrive>().horizontalInput;
         meshes.transform.localEulerAngles = new Vector3(meshes.transform.localEulerAngles.x, meshes.transform.localEulerAngles.y, -horizontalInput * 7 * bikeAcceleration);
+        
     }
 }
